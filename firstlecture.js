42907628286f88ee8e4f6b83179c932caec12716
Uttkarshchambiyal <uttkarshchambiyal26@gmail.com>
2026-01-23 00:41:04 +0530

    Add first JavaScript lecture files

---
+++ firstlecture.js
@@ -0,0 +1,23 @@
+// // add in js 
+// let a = 0.3;
+// let b = 0.1;
+// console.log(a + b);
+// // subtract in js
+// let a1 = 3;
+// let b1 = 2;
+// console.log(a1 - b1);
+// // multiplication in js 
+// let a2 = 0.3;
+// let b2 = 0.1;
+// console.log(a2 * b2);
+// // divide in js
+// let a3 = 3;
+// let b3 = 2;
+// console.log(a3 / b3);
+const student = {
+    fullName: "me",
+    age: 20,
+    cgpa: 5.0,
+    ispass: true,
+};
+console.log(student.age);
